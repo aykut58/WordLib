2c3e75f733b0fb5baa691270b5a4348a902f5c6a
User <user>
2021-03-16 00:34:27 +0300

    Added token_filter

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from flask import Flask
 from db import db,db_url
 from model import User,Category
 from api import register_blueprint,category_blueprint
+from security import token_filter
 
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]=db_url
 db.init_app(app)
 app.register_blueprint(register_blueprint)
 app.register_blueprint(category_blueprint)
+app.before_request(token_filter)
 with app.app_context():
     db.create_all()
 app.run(host="0.0.0.0")
--- security.py
+++ security.py
@@ -1,9 +1,13 @@
 import hashlib
 import jwt
 import datetime
+from flask import request,jsonify
+from repository import UserRepository
+from jwt.exceptions import DecodeError,ExpiredSignatureError
 
 token_key="key"
 token_algorithm="HS256"
+user_repository=UserRepository()
 
 def hash_password(password):
     return hashlib.sha512(password.encode()).hexdigest()
@@ -11,3 +15,20 @@ def hash_password(password):
 def create_token(username):
     expire=round((datetime.datetime.now()+datetime.timedelta(days=30)).timestamp())
     return jwt.encode({"username":username,"exp":expire},token_key,token_algorithm)
+
+def token_filter():
+    if request.path!="/login" and request.path!="/register" and not request.path.startswith("/activate") and request.path!="/token-check":
+        if "token" in request.headers:
+            try:
+                token=jwt.decode(request.headers.get("token"),token_key,token_algorithm)
+            except DecodeError:
+                return jsonify({"Error Message":"Invalid Token"}),400
+            except ExpiredSignatureError:
+                return jsonify({"Error Message":"Expired Token"}),401
+            username=token.get("username")
+            if user_repository.exists_by_username(username):
+                user=user_repository.get_by_username(username)
+            else:
+                return jsonify({"Error Message":"User not Found"}),401
+        else:
+            return jsonify({"Error Message":"Token not Found"}),401
