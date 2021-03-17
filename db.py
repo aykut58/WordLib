98a1fdc9af6486265ad7fb3939e863a45c491ae3
User <user>
2021-03-17 17:26:30 +0300

    Changed db url

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists,create_database
 
-db_url="mysql+mysqlconnector://root:28R35M5qyw7nBDsQ@wordlib.c9khqljdws4k.us-east-1.rds.amazonaws.com:3306/wordlib"
+db_url="mysql+mysqlconnector://root:28R35M5qyw7nBDsQ@wordlib.cllrqf9adx3k.us-east-1.rds.amazonaws.com:3306/wordlib"
 db=SQLAlchemy()
 if not database_exists(db_url):
     create_database(db_url)
