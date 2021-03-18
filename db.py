22a9ac8ed792bae01f4007cfdd43dc689da7b7c5
User <user>
2021-03-19 02:05:57 +0300

    Modified db.py

--- db.py
+++ db.py
@@ -1,7 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists,create_database
+import sys
 
-db_url="mysql+mysqlconnector://root:28R35M5qyw7nBDsQ@wordlib.cllrqf9adx3k.us-east-1.rds.amazonaws.com:3306/wordlib"
+if len(sys.argv)>1:
+    host=sys.argv[1]
+    password=sys.argv[2]
+else:
+    host="localhost"
+    password="password"
+db_url="mysql+mysqlconnector://root:"+password+"@"+host+":3306/wordlib"
+print(db_url)
 db=SQLAlchemy()
 if not database_exists(db_url):
     create_database(db_url)
