86ff0e4298d28a139c3f1786138cb83c83fe846b
User <user>
2021-03-19 02:10:02 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from db import db,db_url
 from model import User,Category
 from api import register_blueprint,category_blueprint,user_blueprint
 from security import token_filter
+import sys
 
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]=db_url
@@ -13,4 +14,7 @@ app.register_blueprint(user_blueprint)
 app.before_request(token_filter)
 with app.app_context():
     db.create_all()
-app.run(host="0.0.0.0")
+if len(sys.argv)>1:
+    app.run(host="0.0.0.0")
+else:
+    app.run(debug=True)
