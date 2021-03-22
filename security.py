8f78d6f618f2d88729d896f00144de3f2ad06f3e
User <user>
2021-03-22 23:55:10 +0300

    Added Authorization filter

--- security.py
+++ security.py
@@ -2,16 +2,17 @@ import hashlib
 import jwt
 import datetime
 from flask import request,jsonify
-from repository import UserRepository
+from repository import UserRepository,AdminRepository
 from jwt.exceptions import DecodeError,ExpiredSignatureError
 
 token_key="7Kc8QdRBrFuRVnBS"
 token_algorithm="HS256"
 user_repository=UserRepository()
+admin_repository=AdminRepository()
 
 def is_token_valid(token):
     try:
-        jwt.decode(request.headers.get("token"),token_key,token_algorithm)
+        jwt.decode(token,token_key,token_algorithm)
         return True
     except:
         return False
@@ -19,12 +20,12 @@ def is_token_valid(token):
 def hash_password(password):
     return hashlib.sha512(password.encode()).hexdigest()
 
-def create_token(username):
+def create_token(user):
     expire=round((datetime.datetime.now()+datetime.timedelta(days=30)).timestamp())
-    return jwt.encode({"username":username,"exp":expire},token_key,token_algorithm)
+    return jwt.encode({"username":user.username,"exp":expire,"usertype":type(user).__name__},token_key,token_algorithm)
 
 def token_filter():
-    if request.path!="/login" and request.path!="/register" and not request.path.startswith("/activate") and request.path!="/token-check":
+    if request.path!="/login" and request.path!="/register" and not request.path.startswith("/activate") and request.path!="/token-check" and request.path!="/admin-login":
         if "token" in request.headers:
             try:
                 token=jwt.decode(request.headers.get("token"),token_key,token_algorithm)
@@ -33,7 +34,14 @@ def token_filter():
             except ExpiredSignatureError:
                 return jsonify({"Error Message":"Expired Token"}),401
             username=token.get("username")
-            if not user_repository.exists_by_username(username):
-                return jsonify({"Error Message":"User not Found"}),401
+            usertype=token.get("usertype")
+            if usertype=="Admin":
+                if not admin_repository.exists_by_username(username):
+                    return jsonify({"Error Message":"User not Found"}),401
+            elif usertype=="User":
+                if not user_repository.exists_by_username(username):
+                    return jsonify({"Error Message":"User not Found"}),401
+                if request.method=="DELETE" or request.method=="POST" or request.method=="PUT":
+                    return jsonify({"Error Message":"Authorization Error"}),403
         else:
             return jsonify({"Error Message":"Token not Found"}),401
