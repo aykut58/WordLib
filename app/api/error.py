834669be121c60d9044c798c83b6b6b3f9fa3ff7
User <user>
2021-04-06 23:38:57 +0300

    Modified error.py Fixed bug

--- app/api/error.py
+++ app/api/error.py
@@ -6,10 +6,9 @@ from app import app
 def errorhandler(error):
     if isinstance(error,NoResultFound):
         return jsonify({"Message":"No Entity Found by This id"}),400
-    if error.code==404:
-        return jsonify({"Message":"URL Not Found"}),404
-    if error.code==500:
-        return jsonify({"Message":"Something went wrong on server"}),500
-    if error.code==405:
-        return jsonify({"Message":"Method not Allowed"}),405
-    return jsonify({"Message":"Error"}),error.code
+    if hasattr(error,"code"):
+        if error.code==404:
+            return jsonify({"Message":"URL Not Found"}),404
+        if error.code==405:
+            return jsonify({"Message":"Method not Allowed"}),405
+    return jsonify({"Message":"Something Went Wrong on Server"}),500
