351500ecb4a0993ecbad0418e7b8bb4d1a08bc34
user <user>
2021-04-29 01:20:32 +0300

    Add CategorySerializer

--- app/serializer.py
+++ app/serializer.py
@@ -1,9 +1,15 @@
-from .model import Word
+from .model import Category,Word
 from flask_marshmallow import Marshmallow
 
 marshmallow=Marshmallow()
 
+class CategorySerializer(marshmallow.Schema):
+    class Meta:
+        model=Category
+        fields=("id","name")
+
 class WordSerializer(marshmallow.Schema):
     class Meta:
         model=Word
-        fields=("id","turkish","english")
+        fields=("id","turkish","english","category")
+    category=marshmallow.Nested(CategorySerializer)
