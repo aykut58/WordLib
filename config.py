c224067de13b04ff972ddae0a9a62cc1662cfeb3
user <user>
2021-05-05 02:59:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     ENV=os.getenv("ENV","development")
     DEBUG=True if ENV=="development" else False
     JWT_KEY=os.getenv("JWT_KEY","key")
-    SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://root:"+os.getenv("DB_PASSWORD","password")+"@"+os.getenv("DB_HOST","localhost")+":3306/wordlib"
+    SQLALCHEMY_DATABASE_URI="mysql://root:"+os.getenv("DB_PASSWORD","password")+"@"+os.getenv("DB_HOST","localhost")+":3306/wordlib"
     SQLALCHEMY_TRACK_MODIFICATIONS=True if ENV=="development" else False
     HOST="127.0.0.1" if ENV=="development" else "0.0.0.0"
